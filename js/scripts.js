65407a701dfb27b43efd01d55488489439bb61e4
sunflwoer519 <92281084+sunflower519sf@users.noreply.github.com>
2024-01-12 23:37:01 +0800

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,72 @@
 const audioButton = document.getElementById('startButton');
-let askCount = 0;
 const asks = document.querySelectorAll(".answer");
+
+let askCount = 0;
+
+
+
 
 function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
 }  
 
+function askEnd() {
+    document.querySelector(".pageAsk").style.display = "none";
+
+}
+
+function askError() {
+    document.querySelector(".pageStart").style.display = "block";
+    document.querySelector(".pageAsk").style.display = "none";
+    askCount = 0;
+}
+
 function askReplace() {
-    fetch('setting.json')
-    .then(response => response.json())
-    .then(data => {
-    // 在這裡處理從 JSON 中獲取的數據
-        console.log(data);
-        const ansData = data.ask[askCount].ans1;
+
+    if (askCount < data.ask.length){
+        const ansData = data.ask[askCount].ans;
+        window.askAns = data.ask[askCount].ans[0];
         shuffleArray(ansData);
 
-        document.querySelector(".ask").innerText = data.ask[askCount].ask1;
+        document.querySelector(".ask").innerText = data.ask[askCount].ask;
         asks[0].innerText = ansData[0];
         asks[1].innerText = ansData[1];
 
-        if (data.ask > askCount){
-            askCount = askCount + 1;
-        } else {
-            askEnd();
-        }
-        
-    })
-    .catch(error => console.error('取得題目資料時出現錯誤:', error));
+        askCount++;
+    } else {
+        askEnd();
+    }
 }
 
-function askEnd() {
-
-}
 
 document.getElementById('startButton').addEventListener('click', function() {
     document.querySelector(".pageStart").style.display = "none";
     document.querySelector(".pageAsk").style.display = "block";
 
-    askReplace();
+    fetch('setting.json')
+    .then(response => response.json())
+    .then(data => {
+        window.data = data;
+        askReplace();
+
+    })
+    .catch(error => console.error('取得題目資料時出現錯誤:', error));
 });
 
 asks.forEach(button => {
-    button.addEventListener('click', () => {
-        console.log('按鈕被點擊了！');
+    button.addEventListener('click', (a) => {
+        if (a.currentTarget.innerText == askAns) {
+            console.log('答對囉！', a.currentTarget.innerText);
+            
+            askReplace();
+        } else {
+            console.log('答錯囉！')
+            askError()
+        }
+        
 
     });
 });
