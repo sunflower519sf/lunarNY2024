0e069bf380e326d15178b6a79f74dd9b905f6be0
sunflwoer519 <92281084+sunflower519sf@users.noreply.github.com>
2024-02-05 16:10:50 +0800

    Add files via upload

--- js/end.js
+++ js/end.js
@@ -1,6 +1,8 @@
 var words = '無法讀取資料';
 let index = 0, num = 0, showRun = false;
 const imgPath = "img/end/"
+var music = document.getElementById('music');
+
 
 fetch('setting.json')
 .then(response => response.json())
@@ -21,18 +23,18 @@ function wordShow() {
         let part = words.substr(0, index+1);
         document.querySelector('.word').textContent = part;
         index++;
-        (index < words.length) ? wordShow() : setTimeout(wordDel, 5000);
+        (index < words.length) ? wordShow() : setTimeout(wordDel, 10000);
     }, 100);
 }
 
 function wordDel() {
-    document.querySelectorAll('.fadeOut').forEach(element => {element.style.animation = 'fadeOut 10s forwards';});
+    document.querySelectorAll('.fadeOut').forEach(element => {element.style.animation = 'fadeOut 5s forwards';});
     setTimeout(() => {
         index = 0;
         showRun = false;
         num += 1;
         (num < data.end.length) ? go() : showEnd();
-    }, 10000);
+    }, 5000);
     
 }
 
@@ -40,11 +42,11 @@ function go(){
     if (!showRun) {
         words = "";
         document.querySelector('.word').textContent = "";
+        document.getElementById('end-img').src = imgPath + data.end[num][0]
         document.querySelectorAll('.fadeOut').forEach(element => {element.style.animation = 'none';});
         for(j=1;j < data.end[num].length;j++) {
             words += data.end[num][j] + "\n";
         }
-        document.getElementById('end-img').src = imgPath + data.end[num][0]
         wordShow();
     }
 }
